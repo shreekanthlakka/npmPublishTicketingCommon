cfdcab8a9ac4fe7bc694936f324bc7f279d2b6b9
shreekanthlakka <shreekanth.lakka@hotmail.com>
2024-07-03 13:46:33 +0530

    changed version

---
+++ src/leetcode/types/languages.js
@@ -0,0 +1,11 @@
+const Languages = Object.freeze({
+    JAVASCRIPT: "javascript",
+    PYTHON:"python",
+    RUBY:"ruby",
+    JAVA:"java",
+    CSHARP:"csharp",
+    CPLUSPLUS:"cplusplus",
+    
+})
+
+export {Languages}
