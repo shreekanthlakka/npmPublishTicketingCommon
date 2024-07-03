974bb049c320c27e02070a536f6c73b662588e68
shreekanthlakka <shreekanth.lakka@hotmail.com>
2024-07-03 14:28:46 +0530

    validation result

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 export * from "./src/utils/CustomError.js";
 export * from "./src/utils/CustomResponse.js";
 export * from "./src/utils/asyncHandler.js";
+export * from "./src/utils/handleValidationErrors.js";
 export * from "./src/middlewares/isLoggedIn.js";
 export * from "./src/events/base-listener.js";
 export * from "./src/events/base-publisher.js";
