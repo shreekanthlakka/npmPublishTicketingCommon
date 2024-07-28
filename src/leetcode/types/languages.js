2361bf4a48402c11994e04113da3515604be7c3f
shreekanthlakka <shreekanthlakka@hotmail.com>
2024-07-28 12:25:36 +0530

    added command and base images

--- src/leetcode/types/languages.js
+++ src/leetcode/types/languages.js
@@ -7,10 +7,22 @@ const Languages = Object.freeze({
     CPLUSPLUS: "cplusplus",
 });
 
-const LanguageBaseImages = Object.freeze({
-    javascript: "",
-    cplusplus: "",
-    rust: "",
+const BaseImages = Object.freeze({
+    javascript: "node:alpine",
+    python: "python:alpine",
+    ruby: "ruby:alpine",
+    java: "openjdk:alpine",
+    rust: "rust",
+    cpp: "gcc:latest",
 });
 
-export { Languages, LanguageBaseImages };
+const Commend = Object.freeze({
+    javascript: ["node", "/scripts/index.js"],
+    python: ["python", "/scripts/index.py"],
+    ruby: ["ruby", "/scripts/index.rb"],
+    java: ["java", "-jar", "/scripts/index.jar"],
+    rust: ["cargo", "run", "--bin", "main"],
+    cpp: ["g++", "-o", "main", "/scripts/main.cpp", "&&", "./main"],
+});
+
+export { Languages, BaseImages, Commend };
